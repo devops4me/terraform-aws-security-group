938bab2e57d459dfdbd638e5456dc76ef739adc5
apollo <apolloakora@gmail.com>
2018-11-08 22:42:45 +0000

    added security group test using default vpc sg

--- integration.test.dir/security.group-test.tf
+++ integration.test.dir/security.group-test.tf
@@ -8,12 +8,12 @@ locals
     ecosystem_id = "sgrules-test"
 }
 
-module zero-param-test
+module zero-paramaters
 {
     source = ".."
 }
 
-module vpc-1
+module virtual-private-cloud-1
 {
     source       = "github.com/devops4me/terraform-aws-vpc-subnets"
     in_vpc_cidr  = "10.123.0.0/16"
@@ -24,12 +24,12 @@ module security-group-test-1
 {
     source         = ".."
     in_ingress     = [ "ssh", "http", "https" ]
-    in_vpc_id      = "${ module.vpc-1.out_vpc_id }"
+    in_vpc_id      = "${ module.virtual-private-cloud-1.out_vpc_id }"
     in_use_default = "true"
     in_ecosystem   = "${ local.ecosystem_id }-01"
 }
 
-module vpc-2
+module virtual-private-cloud-2
 {
     source       = "github.com/devops4me/terraform-aws-vpc-subnets"
     in_vpc_cidr  = "10.124.0.0/16"
@@ -41,9 +41,23 @@ module security-group-test-2
     source         = ".."
     in_ingress     = [ "elasticsearch", "ssh", "java" ]
     in_egress      = [ "all-traffic" ]
-    in_vpc_id      = "${ module.vpc-2.out_vpc_id }"
+    in_vpc_id      = "${ module.virtual-private-cloud-2.out_vpc_id }"
     in_use_default = "false"
     in_ecosystem   = "${ local.ecosystem_id }-02"
+}
+
+# = ===
+# = This security group test
+#      - does not specify egress rules (expecting all-traffic default).
+#      - latches onto the default VPC security group ( in_use_default = true )
+# = ===
+module security-group-test-3
+{
+    source         = "github.com/devops4me/terraform-aws-security-group"
+    in_ingress     = [ "ssh", "http", "https" ]
+    in_vpc_id      = "${ module.virtual-private-cloud-2.out_vpc_id }"
+    in_use_default = "true"
+    in_ecosystem   = "${ local.ecosystem_id }"
 }
 
 output security_group_id
