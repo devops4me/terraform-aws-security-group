f4a7adf6ecf0588c53c64a8394bb5baa67eaed8f
apollo <apolloakora@gmail.com>
2018-11-22 22:03:06 +0000

    Fixed output bug.

--- integration.test.dir/security.group-test.tf
+++ integration.test.dir/security.group-test.tf
@@ -64,7 +64,12 @@ module security-group-test-2b
     in_ecosystem   = "${ local.ecosystem_id }-2b"
 }
 
-output security_group_id
+output security_group_id_1a
 {
-    value = "${module.security-group-test-1.out_security_group_id}"
+    value = "${module.security-group-test-1a.out_security_group_id}"
+}
+
+output security_group_id_2b
+{
+    value = "${module.security-group-test-2b.out_security_group_id}"
 }
