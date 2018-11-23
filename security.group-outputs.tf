09927ddfbbe7ea16834a7429095c7a56ad3b0226
apollo <apolloakora@gmail.com>
2018-11-23 12:00:50 +0000

    Used concat to workaround element and empty list error.

--- security.group-outputs.tf
+++ security.group-outputs.tf
@@ -3,12 +3,19 @@
 ################ Module [[[security group]]] Output Variables List. ########
 ################ ################################################## ########
 
-### ################################ ###
-### [[output]] out_security_group_id ###
-### ################################ ###
 
+/*
+ | --
+ | -- element: element() may not be used with an empty list
+ | --
+ | --
+*/
 output out_security_group_id
 {
     description = "The string ID of either the default security group or the just created new one."
-    value       = "${ var.in_use_default ? element( aws_default_security_group.default.*.id, 0 ) : element( aws_security_group.new.*.id, 0 ) }"
+#######    value       = "${ var.in_use_default ? element( aws_default_security_group.default.*.id, 0 ) : element( aws_security_group.new.*.id, 0 ) }"
+    value       = "${ var.in_use_default ? element( concat( aws_default_security_group.default.*.id, list("") ), 0 ) : element( concat( aws_security_group.new.*.id, list("") ), 0 ) }"
 }
+
+
+########### public_ip_address_id = "${length(azurerm_public_ip.virtual_machine_public_ip.*.id) > 0 ? element(concat(azurerm_public_ip.virtual_machine_public_ip.*.id, list("")), count.index) : ""}"
