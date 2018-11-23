201c390be82035fb466205fc30ce410a388621f5
apollo <apolloakora@gmail.com>
2018-11-23 11:37:48 +0000

    Used splat syntax and element to push out security group id.

--- security.group-outputs.tf
+++ security.group-outputs.tf
@@ -10,5 +10,5 @@
 output out_security_group_id
 {
     description = "The string ID of either the default security group or the just created new one."
-    value       = "${ var.in_use_default ? aws_default_security_group.default.id : aws_security_group.new.id }"
+    value       = "${ var.in_use_default ? element( aws_default_security_group.default.*.id, 0 ) : element( aws_security_group.new.*.id, 0 ) }"
 }
