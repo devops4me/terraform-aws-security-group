f44986a358fb818e52b4b830bf577c581acd3442
Apollo Akora <apolloakora@gmail.com>
2019-07-22 15:52:23 +0100

    correcting the complex type in terraform 0.12

--- security.group-rules.tf
+++ security.group-rules.tf
@@ -28,11 +28,8 @@ variable rules {
     sonar = list(string)
     postgres = list(string)
   })
-}
 
-
-locals {
-  rules = {
+default = {
         http  = [  80,  80, "tcp", "http plaintext" ]
         all-traffic   = [ -1, -1,    "-1",   "All protocols" ]
         ecs          = [ 32768, 61000, "tcp",   "cluster comms" ]
@@ -41,8 +38,10 @@ locals {
         jenkins = [ 8080, 8080, "tcp", "jenkins ci"    ]
         sonar   = [ 9000, 9000, "tcp", "sonarqube metrics" ]
         postgres   = [  5432,  5432, "tcp",  "postgresql db" ]
-  }
 }
+
+}
+
 
 
 /*
