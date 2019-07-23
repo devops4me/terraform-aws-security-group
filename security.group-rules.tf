beb1b50cbd90b5376069b588ab499f3967286d65
Apollo Akora <apolloakora@gmail.com>
2019-07-23 11:11:15 +0100

    moving from list of any to list of strings

--- security.group-rules.tf
+++ security.group-rules.tf
@@ -32,8 +32,23 @@
 
 variable rules {
 
-    type = map( string, list(any) )
+    type = map( string, list( string ) )
 
+    default = {
+        "https"       = [ "443", "443", "tcp",   "http secured" ]
+        "http"        = [  "80",  "80", "tcp", "http plaintext" ]
+        "all-traffic" = [ "-1", "-1",    "-1",   "All protocols" ]
+        "ecs"         = [ "32768", "61000", "tcp",   "cluster comms" ]
+        "docker"      = [ "5000",   "5000", "tcp", "docker registry" ]
+        "java"        = [ "8080", "8080", "tcp", "java port"     ]
+        "jenkins"     = [ "8080", "8080", "tcp", "jenkins ci"    ]
+        "sonar"       = [ "9000", "9000", "tcp", "sonarqube metrics" ]
+        "postgres"    = [  "5432",  "5432", "tcp",  "postgresql db" ]
+    }
+}
+
+
+/*
     default = {
         "https"       = [ 443, 443, "tcp",   "http secured" ]
         "http"        = [  80,  80, "tcp", "http plaintext" ]
@@ -45,7 +60,7 @@ variable rules {
         "sonar"       = [ 9000, 9000, "tcp", "sonarqube metrics" ]
         "postgres"    = [  5432,  5432, "tcp",  "postgresql db" ]
     }
-}
+*/
 
 
 /*
